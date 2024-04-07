5bcd4ee7a33f72c2ebc22ffd3ae32fa32538691a
Francisco Llinin <pacoflores1999@gmail.com>
2024-04-06 22:08:38 -0500

    Normalizate

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,8 +1,6 @@
 class Feature < ApplicationRecord
   has_many :comments
 
- 
-
   validates :title, :url, :place, :mag_type, presence: true
   validates :magnitude, inclusion: { in: -1.0..10.0 }
   validates :latitude, inclusion: { in: -90.0..90.0 }
